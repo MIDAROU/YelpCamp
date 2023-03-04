915f52247cde228bbd1a97930c9f7ed58d5892a6
MIDAROU <mahdibenmhenni0@gmail.com>
2023-03-04 16:05:39 +0100

    cleaned up some code

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 const express = require("express");
 const ConnectDB = require("./Config/Database");
 const app = express();
+const path = path();
 
 const UsersRoutes = require("./Routes/Users");
 const CommentsRoutes = require("./Routes/Comments");
 const CampgroundsRoutes = require("./Routes/Campgrounds");
+const path = require("path");
 
 //PORT
 const PORT = process.env.PORT || 5050;
@@ -20,6 +22,17 @@ app.use(express.json());
 app.use("/api/users", UsersRoutes);
 app.use("/api/comments", CommentsRoutes);
 app.use("/api/campgrounds", CampgroundsRoutes);
+
+//SERVING THE FRONTEND
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", function (_, res) {
+	res.sendFile(
+		path.join(__dirname, "./client/build/index.html"),
+		function (err) {
+			res.status(500).send(err);
+		}
+	);
+});
 
 //SERVER LISTEN
 app.listen(PORT, () => {
